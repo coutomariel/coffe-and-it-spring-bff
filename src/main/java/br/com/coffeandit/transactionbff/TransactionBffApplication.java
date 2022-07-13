2d522bbd0edecedfaa74cdec85a9358d294050bd
mariel <coutomariel@gmail.com>
2022-07-13 11:33:39 -0300

    Redis config

--- src/main/java/br/com/coffeandit/transactionbff/TransactionBffApplication.java
+++ src/main/java/br/com/coffeandit/transactionbff/TransactionBffApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation APIs v1.0"))
+@EnableRedisRepositories(basePackages =  {"br.com.coffeandit.transactionbff.redis"})
 public class TransactionBffApplication {
 
 	public static void main(String[] args) {
